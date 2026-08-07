fb964540439df161ac7a41d4e35ea502a40b54d1
Bogdy555 <florescubogdan555@gmail.com>
2026-08-07 20:54:56 +0300

    Better hashing!

--- BFW/Sources/BFW_Cryptography.cpp
+++ BFW/Sources/BFW_Cryptography.cpp
@@ -4,12 +4,32 @@
 
 const uint64_t BFW_API BFW::Cryptography::HashA(const BFW_CHAR_TYPE_A* _Buffer, const size_t _Length)
 {
-	return std::hash<std::string_view>()(std::string_view(_Buffer, _Length));
+	uint64_t _Result = 0x09E3779B185EBCA8;
+	uint64_t _Prime = 0xC2B2AE3D27D4EB4F;
+
+	for (size_t _Index = 0; _Index < _Length; _Index++)
+	{
+		_Result ^= (uint64_t)(_Buffer[_Index]) * _Prime;
+		_Result = (_Result << 27) | (_Result > 37);
+		_Result *= _Prime;
+	}
+
+	return _Result;
 }
 
 const uint64_t BFW_API BFW::Cryptography::HashW(const BFW_CHAR_TYPE_W* _Buffer, const size_t _Length)
 {
-	return std::hash<std::wstring_view>()(std::wstring_view(_Buffer, _Length));
+	uint64_t _Result = 0x09E3779B185EBCA8;
+	uint64_t _Prime = 0xC2B2AE3D27D4EB4F;
+
+	for (size_t _Index = 0; _Index < _Length; _Index++)
+	{
+		_Result ^= (uint64_t)(_Buffer[_Index]) * _Prime;
+		_Result = (_Result << 27) | (_Result > 37);
+		_Result *= _Prime;
+	}
+
+	return _Result;
 }
 
 const uint64_t BFW_API BFW::Cryptography::Hash(const BFW_CHAR_TYPE* _Buffer, const size_t _Length)
