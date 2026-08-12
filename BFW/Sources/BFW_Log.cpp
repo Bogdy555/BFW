b7fe3ddcab46de66385a73a8abb9c0caa3870b14
Bogdy555 <florescubogdan555@gmail.com>
2026-08-12 22:55:07 +0300

    Oops...

--- BFW/Sources/BFW_Log.cpp
+++ BFW/Sources/BFW_Log.cpp
@@ -62,9 +62,9 @@ const bool BFW_API BFW::Log::Init()
 
 	BFW_DEBUG_CALL(uint64_t _TimeStamp = Time::GetTimeStamp());
 
-	BFW_DEBUG_CALL(LoggingFile.open(BFW_STRING_TYPE(BFW_STRING_PREFIX(".\\BFW_Log_LoggingFile ")) + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".txt"), std::ios::binary));
+	BFW_DEBUG_CALL(LoggingFile.open(BFW_STRING_TYPE(BFW_STRING_PREFIX("./BFW_Log_LoggingFile ")) + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".txt"), std::ios::binary));
 
-	BFW_DEBUG_CALL(ProfilingFile.open(BFW_STRING_TYPE(BFW_STRING_PREFIX(".\\BFW_Log_ProfilingFile ")) + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".json"), std::ios::binary));
+	BFW_DEBUG_CALL(ProfilingFile.open(BFW_STRING_TYPE(BFW_STRING_PREFIX("./BFW_Log_ProfilingFile ")) + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".json"), std::ios::binary));
 	BFW_DEBUG_CALL
 	(
 		ProfilingJson->SetObject();
@@ -108,9 +108,9 @@ const bool BFW_API BFW::Log::Init()
 {
 	BFW_DEBUG_CALL(uint64_t _TimeStamp = Time::GetTimeStamp());
 
-	BFW_DEBUG_CALL(LoggingFile.open(BFW_STRING_TYPE(BFW_STRING_PREFIX(".\\BFW_Log_LoggingFile ")) + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".txt"), std::ios::binary));
+	BFW_DEBUG_CALL(LoggingFile.open(BFW_STRING_TYPE(BFW_STRING_PREFIX("./BFW_Log_LoggingFile ")) + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".txt"), std::ios::binary));
 
-	BFW_DEBUG_CALL(ProfilingFile.open(BFW_STRING_TYPE(BFW_STRING_PREFIX(".\\BFW_Log_ProfilingFile ")) + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".json"), std::ios::binary));
+	BFW_DEBUG_CALL(ProfilingFile.open(BFW_STRING_TYPE(BFW_STRING_PREFIX("./BFW_Log_ProfilingFile ")) + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".json"), std::ios::binary));
 	BFW_DEBUG_CALL
 	(
 		ProfilingJson->SetObject();
@@ -150,9 +150,9 @@ const bool BFW_API BFW::Log::Init(const size_t _BaudRate, const BFW_STRING_TYPE&
 
 	BFW_DEBUG_CALL(uint64_t _TimeStamp = Time::GetTimeStamp());
 
-	BFW_DEBUG_CALL(LoggingFile.open(_SDCardPath + BFW_STRING_PREFIX("\\BFW_Log_LoggingFile ") + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".txt"), std::ios::binary));
+	BFW_DEBUG_CALL(LoggingFile.open(_SDCardPath + BFW_STRING_PREFIX("/BFW_Log_LoggingFile ") + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".txt"), std::ios::binary));
 
-	BFW_DEBUG_CALL(ProfilingFile.open(_SDCardPath + BFW_STRING_PREFIX("\\BFW_Log_ProfilingFile ") + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".json"), std::ios::binary));
+	BFW_DEBUG_CALL(ProfilingFile.open(_SDCardPath + BFW_STRING_PREFIX("/BFW_Log_ProfilingFile ") + BFW_TO_STRING(_TimeStamp) + BFW_STRING_PREFIX(".json"), std::ios::binary));
 	BFW_DEBUG_CALL
 	(
 		ProfilingJson->SetObject();
