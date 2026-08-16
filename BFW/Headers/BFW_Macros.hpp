4f65c899b97a8197f799a49518e4e27348f6e1c5
Bogdy555 <florescubogdan555@gmail.com>
2026-08-16 17:06:00 +0300

    Fixed file logging!

--- BFW/Headers/BFW_Macros.hpp
+++ BFW/Headers/BFW_Macros.hpp
@@ -379,8 +379,8 @@
 
 #ifdef BFW_DEBUG
 
-#define BFW_LOG_TO_FILE(X) *BFW::Log::LoggingFile << X
-#define BFW_LOG_LINE_TO_FILE(X) *BFW::Log::LoggingFile << X << L'\n'
+#define BFW_LOG_TO_FILE(X) BFW::Log::LoggingFile << X
+#define BFW_LOG_LINE_TO_FILE(X) BFW::Log::LoggingFile << X << L'\n'
 
 #endif
 
